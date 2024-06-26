b08a298092ee991a47398bb18739770eaebe4e2a
penglb3 <penglb3@node6.dfs-perf-test.nsccgz-storage-pg0.utah.cloudlab.us>
2024-06-26 06:10:16 -0600

    Try to start >2 servers

--- server-runner.sh
+++ server-runner.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+rm -rf /tmp/deltafs_outputs
 OMPI_COMM_WORLD_NODE_RANK=${OMPI_COMM_WORLD_NODE_RANK:-0}
 IP="10.10.1.7"
 BASE_PORT=10101
 PORT=$(($OMPI_COMM_WORLD_NODE_RANK + $BASE_PORT))
-export DELTAFS_MetadataSrvAddrs=$IP:$PORT
+LAST_PORT=$(($OMPI_COMM_WORLD_SIZE + $BASE_PORT - 1))
+FORMAT="$IP:%g"
+export DELTAFS_MetadataSrvAddrs=$((seq -s'&' -f "$FORMAT" $BASE_PORT $LAST_PORT))
 numactl --all -C $OMPI_COMM_WORLD_NODE_RANK -- deltafs-srvr -v=1 -logtostderr
--- start-server.sh
+++ start-server.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -xe
 
+rm -rf /tmp/deltafs_outputs
 OMPI_COMM_WORLD_NODE_RANK=${OMPI_COMM_WORLD_NODE_RANK:-0}
 N_THREADS=${3:-1}
 
 BASE_IP=$1
 BASE_PORT=10101
 N_SERVERS=${2:-1}
-LAST_PORT=$(($NUM_SERVERS + $BASE_PORT - 1))
+LAST_PORT=$(($N_SERVERS + $BASE_PORT - 1))
 PORT=$(($OMPI_COMM_WORLD_NODE_RANK + $BASE_PORT))
 
-export "DELTAFS_MetadataSrvAddrs=$(seq -s'&' -f '${BASE_IP}:%g' $BASE_PORT $LAST_PORT)"
+FORMAT="$BASE_IP:%g"
+
+export DELTAFS_MetadataSrvAddrs=$(seq -s'&' -f "$FORMAT" $BASE_PORT $LAST_PORT)
 export DELTAFS_NumOfMetadataSrvs=$N_SERVERS
 export DELTAFS_NumOfThreads=$N_THREADS
 
-mpirun -n $N_SERVERS\
-numactl --all -C $OMPI_COMM_WORLD_NODE_RANK --\
+mpirun -n $N_SERVERS \
+numactl --all -C 0-$N_SERVERS -- \
 deltafs-srvr -v=1 -logtostderr
