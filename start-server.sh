0336027cdc0625ee593f3df72421d640eb031960
penglb3 <penglb3@node6.dfs-perf-test.nsccgz-storage-pg0.utah.cloudlab.us>
2024-06-26 06:19:32 -0600

    give up on multiple servers lol

--- start-server.sh
+++ start-server.sh
@@ -2,14 +2,13 @@
 set -xe
 
 rm -rf /tmp/deltafs_outputs
-OMPI_COMM_WORLD_NODE_RANK=${OMPI_COMM_WORLD_NODE_RANK:-0}
+
 N_THREADS=${3:-1}
 
 BASE_IP=$1
 BASE_PORT=10101
 N_SERVERS=${2:-1}
 LAST_PORT=$(($N_SERVERS + $BASE_PORT - 1))
-PORT=$(($OMPI_COMM_WORLD_NODE_RANK + $BASE_PORT))
 
 FORMAT="$BASE_IP:%g"
 
@@ -18,5 +17,5 @@ export DELTAFS_NumOfMetadataSrvs=$N_SERVERS
 export DELTAFS_NumOfThreads=$N_THREADS
 
 mpirun -n $N_SERVERS \
-numactl --all -C 0-$N_SERVERS -- \
+# numactl --all -C 0-$(($N_SERVERS - 1)) -- \
 deltafs-srvr -v=1 -logtostderr
